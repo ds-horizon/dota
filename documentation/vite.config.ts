0526156c5c57bed2375df7a4ef509340c3c940f5
yashawasthi10 <74182249+yashawasthi10@users.noreply.github.com>
2025-05-08 23:21:26 +0530

    chore: updated config for vite

--- documentation/vite.config.ts
+++ documentation/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
-  base: '/dota/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
